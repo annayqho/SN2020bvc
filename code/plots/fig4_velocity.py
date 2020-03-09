cef78b0c85f8c20e876cf6d018ad93f434b6da8a
annayqho <annayqho@gmail.com>
2020-03-09 16:14:40 -0700

    add vel and lum tnu plots

---
+++ code/plots/fig4_velocity.py
@@ -0,0 +1,375 @@
+""" Plot of velocity vs. time """
+
+import glob
+import matplotlib
+import matplotlib.pyplot as plt
+plt.rc("font", family="serif")
+plt.rc("text", usetex=True)
+import numpy as np
+from astropy.io import fits as pyfits
+from astropy.table import Table
+from astropy.cosmology import Planck15
+
+# time of the last non-detection
+t0 = 58233.17615 # in MJD
+
+
+DATA_DIR = "/Users/annaho/Dropbox/Projects/Research/SN2020bvc/data"
+
+GRBSN_col = '#e55c30'
+GRBSN_lw = 2
+GRBSN_alpha = 1.0
+
+FASTSN_col = '#84206b'
+FASTSN_lw = 3
+FASTSN_alpha = 0.5
+
+
+def plot_20bvc(ax):
+    dt = np.array([5.7, 6.8, 10, 10.8, 12.8, 13.6, 18.9, 26.9])
+    vel = np.array([2.77,2.32,2.35,2.28,2.25,1.90,1.95,1.98])
+    evel = np.array([0.53, 0.74, 0.46, 0.63, 0.4, 0.39, 0.46, 0.57])
+    ax.errorbar(dt, vel*10, yerr=evel*10,
+            fmt='s', c='k', label="SN2020bvc", 
+            zorder=10, ms=5, lw=1)
+
+def plot_18aaqjovh(ax,background=True):
+    """ """
+    dt = np.array([14,19,20,44])
+    vel = np.array([21394,17443,18380,11229])
+    evel = np.array([4647,2797,5432,3306])
+
+    plt.plot(
+        dt, vel/1E3, 
+        c=FASTSN_col, lw=FASTSN_lw, alpha=FASTSN_alpha, zorder=5,
+        label="Fast Ic-BL")
+    if background is False:
+        ax.text(0.1, 0.8, "ZTF18aaqjovh", fontsize=12, transform=ax.transAxes)
+
+
+def plot_18gep():
+    """ V-band max is 3 days after t_0 """
+    # From early spectra
+    dt_early = np.array([1, 2, 4.2])
+    vel_early = np.array([45000, 33000, 30000]) / 1E3
+
+    # From Ragnhild
+    dt = np.array([4, 9, 11.5, 16.5, 22])
+    vel = np.array([30000, 23000, 24000, 22000, 22000])/1E3
+    evel = np.array([1000, 4000, 2000, 1000, 2000])/1E3
+    el = np.array(['O', 'Fe', 'Fe', 'Fe', 'Fe'])
+
+    plt.plot(
+        np.hstack((dt_early, dt)), np.hstack((vel_early, vel)), 
+        c=FASTSN_col, lw=FASTSN_lw, alpha=FASTSN_alpha, zorder=5,
+        label="Fast Ic-BL")
+    plt.text(dt_early[1]*1.1, vel_early[1], '18gep',
+            horizontalalignment='left', fontsize=12,
+            verticalalignment='bottom')
+
+
+def plot_16asu():
+    """ values from Table 4 of Whitesides et al. 2017
+    
+    for the explosion date, they get 57518.53, 2016 May 10.53 (Section 3.1)
+    let's use the Fe II velocity since that's the closest to what we have
+    the uncertainty on their explosion date is 0.17d
+    """
+    # dt between May 10.53 and May 31.99 (inclusive) = 21.47
+    dt = np.array(
+            [24.97-10.53, 27.36-10.53, 21.47+4.39, 21.47+7.36, 21.47+10.42])
+    vel = np.array(
+            [28.3, 29.5, 25.7, 21.6, 22.0])*1000/1E3
+    evel = np.array(
+            [1.3, 1.4, 0.3, 0.4, 1.3])*1000/1E3
+    plt.plot(
+            dt, vel, c=FASTSN_col, lw=FASTSN_lw, zorder=10, alpha=FASTSN_alpha)
+    plt.text(dt[1]*1.1, vel[1], 'iPTF16asu',
+            horizontalalignment='left', fontsize=12,
+            verticalalignment='bottom')
+
+
+def plot_1998bw(ax, background=False):
+    """ Modjaz et al. 2016
+    offset from Galama 1998 """
+    dat = np.loadtxt(DATA_DIR + "/modjaz_vel.txt", dtype=str, delimiter=';')
+    names = np.array([val.strip() for val in dat[:,0]])
+
+    col = 'lightgrey'
+    if background is False:
+        col = GRBSN_col
+        ax.text(0.1, 0.8, "SN1998bw", fontsize=12, transform=ax.transAxes)
+
+    name = 'sn1998bw'
+    choose = names==name
+    phase = dat[:,1][choose].astype(float)
+    vel = dat[:,2][choose].astype(float)*-1
+    evel = dat[:,3][choose].astype(float)
+    offset = 2450945.7-2450929.41
+    dt = phase+offset
+    ax.plot(dt, vel/1E3, c=col, lw=GRBSN_lw, alpha=GRBSN_alpha)
+
+
+def plot_2006aj(ax, background=False):
+    """ Modjaz et al. 2016
+    offset from Campana 2006 """
+    dat = np.loadtxt(DATA_DIR + "/modjaz_vel.txt", dtype=str, delimiter=';')
+    names = np.array([val.strip() for val in dat[:,0]])
+
+    col = 'lightgrey'
+    if background is False:
+        col = GRBSN_col
+        ax.text(0.1, 0.8, "SN2006aj", fontsize=12, transform=ax.transAxes)
+    name = 'sn2006aj'
+    choose = names==name
+    phase = dat[:,1][choose].astype(float)
+    vel = dat[:,2][choose].astype(float)*-1
+    evel = dat[:,3][choose].astype(float)
+    offset = 2453794.7-2453784.649
+    dt = phase+offset
+    ax.plot(
+            dt, vel/1E3, c=col, lw=GRBSN_lw, alpha=GRBSN_alpha)
+
+
+def plot_2010bh(ax, background):
+    """ Modjaz et al. 2016
+    offset from Bufano 2012 """
+    dat = np.loadtxt(DATA_DIR + "/modjaz_vel.txt", dtype=str, delimiter=';')
+    names = np.array([val.strip() for val in dat[:,0]])
+
+    col = 'lightgrey'
+    if background is False:
+        col = GRBSN_col
+        ax.text(0.1, 0.8, "SN2010bh", fontsize=12, transform=ax.transAxes)
+    name = 'sn2010bh'
+    choose = names==name
+    phase = dat[:,1][choose].astype(float)
+    vel = dat[:,2][choose].astype(float)*-1
+    evel = dat[:,3][choose].astype(float)
+    offset = 8 
+    dt = phase+offset
+    ax.plot(
+            dt, vel/1E3, c=col, lw=GRBSN_lw, alpha=GRBSN_alpha)
+
+
+def plot_2009bb(ax, background):
+    dat = np.loadtxt(DATA_DIR + "/modjaz_vel.txt", dtype=str, delimiter=';')
+    names = np.array([val.strip() for val in dat[:,0]])
+    col = 'lightgrey'
+    if background is False:
+        col = GRBSN_col
+        ax.text(0.1, 0.8, "SN2009bb", fontsize=12, transform=ax.transAxes)
+    name = 'sn2009bb'
+    # explosion epoch: March 19
+    # maximum light: Apr 1 (Soderberg 2009)
+    choose = names == name
+    phase = dat[:,1][choose].astype(float)
+    vel = dat[:,2][choose].astype(float)*-1
+    evel = dat[:,3][choose].astype(float)
+    offset = 13
+    dt = phase+offset
+    ax.plot(
+            dt, vel/1E3, c=col, lw=GRBSN_lw, alpha=GRBSN_alpha)
+
+
+def plot_2012ap(ax, background):
+    dat = np.loadtxt(DATA_DIR + "/modjaz_vel.txt", dtype=str, delimiter=';')
+    names = np.array([val.strip() for val in dat[:,0]])
+    col = 'lightgrey'
+    if background is False:
+        col = GRBSN_col
+        ax.text(0.1, 0.8, "SN2012ap", fontsize=12, transform=ax.transAxes)
+    name = 'sn2012ap'
+    # explosion date: 2012 Feb 5
+    # maximum light: let's use the B-band max from Milisavljevic 2014,
+    # which is on Feb 18.2, so that's 13 days
+    offset = 13
+    choose = names == name
+    phase = dat[:,1][choose].astype(float)
+    vel = dat[:,2][choose].astype(float)*-1
+    evel = dat[:,3][choose].astype(float)
+    dt = phase+offset
+    ax.plot(
+            dt, vel/1E3, c=col, lw=GRBSN_lw, alpha=GRBSN_alpha)
+
+
+def plot_17cw(ax, background):
+    # iPTF17cw
+    # max light: 57760.666 is 8 days before maximum light
+    # explosion time set to be 57750.552
+    # so it's 18 days to maximum
+    col = 'lightgrey'
+    if background is False:
+        col = GRBSN_col
+        ax.text(0.1, 0.8, "iPTF17cw", fontsize=12, transform=ax.transAxes)
+    dt = np.array([17, 43]) + 18 # since maximum light
+    v = np.array([17300, 17500]) / 1E3
+    ax.plot(
+            dt, v, c=col, lw=GRBSN_lw, alpha=GRBSN_alpha)
+
+
+def plot_2003lw():
+    """ Modjaz et al. 2016
+    offset from Malesani 2014"""
+    dat = np.loadtxt(DATA_DIR + "/modjaz_vel.txt", dtype=str, delimiter=';')
+    names = np.array([val.strip() for val in dat[:,0]])
+
+    name = 'sn2003lw'
+    choose = names==name
+    phase = dat[:,1][choose].astype(float)
+    vel = dat[:,2][choose].astype(float)*-1
+    evel = dat[:,3][choose].astype(float)
+    offset = 8 
+    dt = phase+offset
+    plt.plot(
+            dt, vel/1E3, c='#f6d746', lw=3, alpha=0.5)
+    #plt.errorbar(
+    #        dt, vel/1E3, yerr=evel/1E3, marker='D', c='#f6d746', alpha=0.3)
+    plt.text(
+            dt[0], vel[0]/1E3, 'SN2003lw', fontsize=12,
+            verticalalignment='bottom', horizontalalignment='left',
+            label="LLGRB-SNe")
+
+
+def grb171205a():
+    # For GRB171205A, the g-band max is about 12 days after the GRB
+    dat = np.loadtxt(
+            DATA_DIR + "/grb171205a_vel.txt", dtype=float, delimiter=',')
+    dt = dat[:,0]
+    vel = dat[:,1]/1E3
+    #plt.scatter(dt, vel, marker='o', c='grey', alpha=0.5)
+    plt.plot(dt, vel, ls='-', c='#f6d746', lw=3, alpha=0.5, label="LLGRB-SN")
+    plt.text(dt[0], vel[0], 'SN2017iuk', fontsize=12)
+
+
+def plot_12gzk():
+    """ Modjaz et al. 2016 """
+    dat = np.loadtxt(
+            DATA_DIR + "/modjaz_vel.txt", dtype=str, delimiter=';')
+    n = 'PTF12gzk'
+
+    names = np.array([val.strip() for val in dat[:,0]])
+    choose = np.where(names==n)[0]
+
+    name = names[choose]
+    phase = dat[:,1][choose].astype(float)
+    vel = dat[:,2][choose].astype(float)*-1
+    evel = dat[:,4][choose].astype(float)
+
+    # for the dt, use the fact that Ic usually reach V-band max in 12-20 days
+    # to add a sort of uncertainty to the dt, so offset would be 16 +/- 4 days
+    dt = phase+16
+    plt.plot(
+            dt, vel/1E3, c='grey', lw=3)
+    plt.text(dt[1], vel[1]/1E3, 'PTF12gzk',
+            horizontalalignment='right', fontsize=12,
+            verticalalignment='bottom')
+
+
+
+def plot_population():
+    """ Modjaz et al. 2016 """
+    inputf = pyfits.open(DATA_DIR + "/asu.fit")
+    dat = inputf[1].data
+    inputf.close()
+    names = dat['SN']
+    phase = dat['Phase']
+    vel = -1*dat['vabs']/1E3
+
+
+
+
+    # 2008D: measured using He I 5876 lines
+    dt = np.array(
+            [5.75, 6.77, 9.00, 11.66,  
+             22.78, 23.88, 30.56, 32.61, 
+             33.87, 37.79, 49.81, 60.67])
+    v = np.array(
+            [14200, 13300, 12300, 11500, 
+             10600, 10600, 10500, 10400, 
+             10300, 10000, 9300, 8600]) /1E3
+    plt.scatter(dt, v, c='grey')
+    plt.plot(dt, v, c='grey')
+    plt.text(dt[0], v[0], '2008D(HeI)',
+            horizontalalignment='right', fontsize=12,
+            verticalalignment='top', zorder=5)
+
+
+
+    # regular Ic
+    name = 'sn2007gr'
+    # time of max: 2454338.5
+    # time of discovery: 2007 Aug 15.51, expl must be < 5 days before disc
+    # arbitrary rise time of 13.5 is used in Mazzali 2010,
+    # do we use that here
+    offset = 13.5
+    choose = names == name
+    dt = phase[choose]+offset
+    v = vel[choose]
+    plt.plot(dt, v, c='#57106e', label='SN Ic')
+    plt.scatter(dt, v, c='#57106e')
+    plt.text(dt[0], v[0], 'SN2007gr',
+            horizontalalignment='right', fontsize=12,
+            verticalalignment='center')
+
+
+    # regular Ic
+    name = 'sn2005az'
+    # time of max: 2453473
+    # arbitrary rise time of 13.5
+    offset = 13.5
+    choose = names == name
+    dt = phase[choose]+offset
+    v = vel[choose]
+    plt.plot(dt, v, c='#57106e')
+    plt.scatter(dt, v, c='#57106e')
+    plt.text(dt[0], v[0], 'SN2005az',
+            horizontalalignment='right', fontsize=12,
+            verticalalignment='center')
+
+
+
+if __name__=="__main__":
+    fig,axarr = plt.subplots(
+            3, 2, figsize=(6,6), sharex=False, sharey=True)
+
+    for ii,ax in enumerate(axarr.reshape(-1)):
+        plot_20bvc(ax)
+        #plot_18aaqjovh(ax)
+        plot_1998bw(ax, background=True)
+        plot_2006aj(ax, background=True)
+        plot_2010bh(ax, background=True)
+        plot_2009bb(ax, background=True)
+        plot_2012ap(ax, background=True)
+        plot_17cw(ax, background=True)
+        # plot_18gep()
+        # plot_16asu()
+        # grb171205a()
+        # plot_2003lw()
+        # plot_12gzk()
+
+        ax.yaxis.set_tick_params(labelsize=14)
+        ax.xaxis.set_tick_params(labelsize=14)
+        ax.set_yscale('log')
+        ax.yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+        ax.set_xlim(0, 33)
+        ax.set_xticks([0,10,20,30])
+        ax.set_ylim(10, 100)
+
+    plot_1998bw(axarr[0,0], background=False)
+    plot_2010bh(axarr[1,0], background=False)
+    plot_2006aj(axarr[2,0], background=False)
+    plot_2009bb(axarr[0,1], background=False)
+    plot_2012ap(axarr[1,1], background=False)
+    plot_17cw(axarr[2,1], background=False)
+    axarr[2,1].set_xlim(0, 60)
+    axarr[2,1].set_xticks([0, 20, 40, 60])
+    fig.text(0.5, 0.04, r"$\Delta t$ (days)", ha='center', fontsize=16)
+    fig.text(
+            0.04, 0.5, r'Fe II Velocity ($10^3$ km/s)',
+            fontsize=16, rotation='vertical', horizontalalignment='center',
+            verticalalignment='center')
+    fig.subplots_adjust(wspace=0.1, hspace=0.2)
+
+    #plt.show()
+    plt.savefig("vel.png", dpi=500)
---
+++ code/plots/fig7_lum_tnu.py
@@ -0,0 +1,306 @@
+""" Peak lum phase space for Ic-BL SNe """
+
+from matplotlib import rc
+rc("font", family="serif")
+rc("text", usetex=True)
+import matplotlib.pyplot as plt
+import numpy as np
+from astropy.table import Table
+from astropy.cosmology import Planck15
+
+
+smallsize=12
+medsize=14
+bigsize=16
+
+squaresize = 50
+
+
+def ujy_to_flux(ujy, z):
+    d = Planck15.luminosity_distance(z=z).cgs.value
+    return ujy*1E-6*1E-23*4*np.pi*d**2
+
+
+def vel_lines(ax, x, v):
+    """ Equation 16 from Chevalier 1998 
+    
+    Parameters
+    ----------
+    ax: axis to plot the stuff on
+    x: the value of (dt / 1 d) * (nu_p / 5 GHz)
+    v: velocity in units of c
+    """
+    xvals = np.linspace(1,3000)
+    logy = (26) + \
+            (19/9) * np.log10(v) + \
+            (19/9) * np.log10(xvals)
+    yvals = 10**logy
+    ax.plot(xvals, yvals, ls='--', c='k', lw=0.5)
+    rotangle = 65
+    ax.text(
+            x, 4E27, "$R/\Delta t = %sc$" %v, 
+            fontsize=smallsize, rotation=rotangle,
+            horizontalalignment='center', verticalalignment='top')
+    return yvals
+
+
+def mdot_curves(ax, x, y, mdotv):
+    """ 
+    x: x-coordinate for where to put the text
+    y: y-coordinate for where to put the text
+    mdotv: Mdot divided by v, in units of (10^{-4} Msol/yr) / (1000 km/s)
+    x: (dt/1 day) * (nu_p / 5 GHz)
+    """
+    #mdotv = mdotv_scaled * 1000 / 1E-4
+    xvals = np.linspace(1,3000)
+    eps_B = 1/3
+    logy = (19/4) * np.log10(0.0005/eps_B) - (19/4)*np.log10(mdotv) + \
+            (2*19/4)*np.log10(xvals) 
+    yvals = 1E26 * 10**logy
+    ax.plot(xvals, yvals, ls=':', c='k', lw=0.5)
+    rotangle = 84
+    ax.text(
+            x, y, 
+            "$\dot{M}/v = 10^{%s}$" %int(np.log10(mdotv)), 
+            fontsize=smallsize, rotation=rotangle,
+            horizontalalignment='left', verticalalignment='top')
+    return yvals
+
+
+def density_curves(ax, x, ne):
+    """ 
+    Parameters
+    ----------
+    ax: axis to plot the stuff on
+    x: the value of (dt / 1 d) * (nu_p / 5 GHz)
+    v: density in units of parts per cm cubed
+    """
+    xvals = np.linspace(1,3000)
+    logy = (19/22) * np.log10(79) + 26 - (19/22)*np.log10(ne) + \
+            (19/22) * 2 * np.log10(xvals**2 / 22) # divide out 22 days
+    yvals = 10**logy
+    ax.plot(xvals, yvals, ls=':', c='k', lw=0.5)
+    rotangle = 75 
+    ax.text(
+            x, 5E29, "$n_e = 10^{%s} \mathrm{cm}^{-3}$" %int(np.log10(ne)), 
+            fontsize=smallsize, rotation=rotangle,
+            horizontalalignment='left', verticalalignment='top')
+    return yvals
+
+
+def lumtnu(ax):
+    # SN2020bvc
+    tnu = (24)*(3/5)
+    lpeak = 1.7E27
+    ax.scatter(tnu,lpeak,marker='s',c='k',s=100,label=None)
+    ax.arrow(
+            tnu, lpeak, -tnu/4, 0, color='k', 
+            head_length=tnu/10, head_width=lpeak/8)
+    ax.text(
+            tnu, lpeak/1.2, "SN2020bvc", fontsize=medsize,
+            verticalalignment='top',
+            horizontalalignment='center', color='k')
+
+    # ZTF18aaqjovh
+    xmin = 20*(3/5)
+    xmax = 20*(15/5)
+    xavg = (xmin+xmax)/2
+    ymin = ujy_to_flux(20, 0.05403)
+    ymax = ujy_to_flux(30, 0.05403)
+    yavg = (ymin+ymax)/2
+    ax.plot([xmin,xmax],[yavg,yavg],c='k')
+    ax.plot([xavg,xavg],[ymin,ymax],c='k')
+    ax.text(
+            xmax, ymin, "ZTF18aaqjovh", fontsize=medsize,
+            verticalalignment='center',
+            horizontalalignment='left', color='k')
+
+    # iPTF17cw
+    tnu = (16)*(2.8/5)
+    lpeak = 1E28
+    ax.scatter(
+            tnu, lpeak, marker='+', c='k', s=100,
+            label=None)
+    # ax.arrow(
+    #         tnu, lpeak, -tnu/4, 0, color='k', 
+    #         head_length=tnu/10, head_width=lpeak/8)
+    # ax.arrow(
+    #         tnu, lpeak, 0, lpeak/4, color='k', 
+    #         head_length=lpeak/5, head_width=tnu/8)
+    ax.text(
+            tnu, lpeak/1.2, "iPTF17cw", fontsize=medsize,
+            verticalalignment='top',
+            horizontalalignment='left', color='k')
+
+    # PTF14dby
+    tnu = (40)*(7.4/5)
+    lpeak = 2.7E28
+    ax.scatter(
+            tnu, lpeak, marker='+', c='k', s=100,
+            label=None)
+    ax.text(
+            tnu, lpeak/1.2, "PTF14dby", fontsize=medsize,
+            verticalalignment='top',
+            horizontalalignment='left', color='k')
+
+    # PTF11cmh
+    tnu = (100)*(5/5)
+    lpeak = 1E29
+    ax.scatter(
+            tnu, lpeak, marker='+', c='k', s=100,
+            label=None)
+    ax.text(
+            tnu, lpeak*1.1, "PTF11cmh", fontsize=medsize,
+            verticalalignment='bottom',
+            horizontalalignment='left', color='k')
+
+
+    # 11qcj
+    tnu = (100)*(5/5)
+    lpeak = 7E28
+    ax.scatter(
+            tnu, lpeak, marker='+', c='k', s=100,
+            label=None)
+    ax.text(
+            tnu*1.15, lpeak, "PTF11qcj", fontsize=medsize,
+            verticalalignment='center',
+            horizontalalignment='left', color='k')
+
+
+    # 2007bg
+    tnu = (55.9)*(8.46/5)
+    lpeak = 4.1E28
+    ax.scatter(
+            tnu, lpeak, marker='+', c='k', s=100, label=None)
+    ax.text(
+            tnu, lpeak*1.1, "2007bg", fontsize=medsize,
+            verticalalignment='bottom',
+            horizontalalignment='right')
+
+    # SN 2003bg
+    tnu = (35)*(22.5/5)
+    lpeak = 3.9E28
+    ax.scatter(
+            tnu, lpeak, marker='+', c='k', s=100, label=None)
+    ax.text(
+            tnu, lpeak/1.1, "2003bg", fontsize=medsize,
+            verticalalignment='top',
+            horizontalalignment='left')
+
+    # SN 1998bw
+    tnu = (10)*(10/5)
+    lpeak = 8.2E28
+    ax.scatter(
+            tnu, lpeak, marker='s', edgecolor='k', s=squaresize,
+            facecolor='k', label="LLGRB-SN")
+    ax.text(
+            tnu, lpeak*1.2, "1998bw", fontsize=medsize,
+            verticalalignment='bottom',
+            horizontalalignment='center')
+
+    # GRB 171205A
+    tnu = (4.3)*(6/5)
+    dgrb = Planck15.luminosity_distance(z=0.0368).cgs.value
+    # 3 mJy at 6 GHz with the VLA; Laskar et al. 2017
+    lpeak = 3E-3 * 1E-23 * 4 * np.pi * dgrb**2
+    ax.scatter(
+            tnu, lpeak, marker='s', edgecolor='k', s=squaresize,
+            facecolor='k', label=None)
+    ax.text(
+            tnu, lpeak*1.2, "171205A", fontsize=medsize,
+            verticalalignment='bottom',
+            horizontalalignment='center')
+
+
+    # SN 2009bb
+    tnu = (20)*(6/5)
+    lpeak = 3.6E28
+    ax.scatter(
+            tnu, lpeak, marker='+', c='k', s=100, label="Ic-BL no GRB")
+    ax.text(
+            tnu/1.2, lpeak, "2009bb", fontsize=medsize,
+            verticalalignment='center',
+            horizontalalignment='right')
+
+    # SN 2006aj
+    tnu = (5)*(4/5)
+    lpeak = 8.3E27
+    ax.scatter(
+            tnu, lpeak, marker='s', edgecolor='k', s=squaresize,
+            facecolor='k', label=None)
+    ax.text(
+            tnu, lpeak/1.3, "2006aj", fontsize=medsize,
+            verticalalignment='top',
+            horizontalalignment='center')
+
+    # SN 2010bh
+    tnu = (30)*(5/5)
+    lpeak = 1.2E28
+    ax.scatter(
+            tnu, lpeak, marker='s', edgecolor='k', s=squaresize,
+            facecolor='k', label=None)
+    ax.text(
+            tnu, lpeak*1.1, "2010bh", fontsize=medsize,
+            verticalalignment='bottom',
+            horizontalalignment='center')
+
+    # Lines
+    y = vel_lines(ax, 5.5, 1)
+    y = vel_lines(ax, 55, 0.1)
+    y = vel_lines(ax, 550, 0.01)
+
+    # AT2018cow
+    x1 = 22*100/5
+    y1 = 4.4E29
+    ax.scatter(
+            x1, y1, marker='*', s=100, 
+            facecolors='black', edgecolors='black')
+    ax.text(
+            22*100/7, 5.5E29, "AT2018cow", fontsize=medsize, 
+            verticalalignment='bottom', 
+            horizontalalignment='left')
+
+    ax.set_xlim(2, 3000)
+    ax.set_ylim(5E26, 1E30)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.tick_params(axis='both', labelsize=bigsize)
+    ax.set_xlabel(
+        "$(\Delta t/1\,\mathrm{day})(\\nu_p/5\,\mathrm{GHz})$",
+        fontsize=bigsize)
+
+    
+fig,ax = plt.subplots(1,1, figsize=(6,6))
+lumtnu(ax)
+y = mdot_curves(ax, 550, 2.5E29, 100)
+y = mdot_curves(ax, 58, 4E29, 1)
+y = mdot_curves(ax, 5.9, 6.4E29, 0.01)
+#y = mdot_curves(ax, 1800, 1E-4)
+ax.set_ylabel(
+    "Peak Radio Luminosity Density ($\mathrm{erg\,s^{-1}\,Hz^{-1}}$)",
+    fontsize=bigsize)
+#ax.get_yaxis().set_visible(False)
+ax.legend(
+        loc=(0.55,0.3), ncol=1, fontsize=medsize, 
+        columnspacing=0.01, borderpad=0.3)#, columnspacing=0.1)
+#y = mdot_curves(ax, 700, 1E1)
+
+# make a twin axis
+ax2 = ax.twinx()
+ax2.set_ylabel(
+        r"$U/R$ (erg/cm) $\qquad \epsilon_e=\epsilon_B=1/3$", 
+        fontsize=bigsize, rotation=270, labelpad=15.0)
+y_f = lambda y_i: 10**((14/19)*(np.log10(y_i)+14.65))
+ymin, ymax = ax.get_ylim()
+ax2.set_ylim((y_f(ymin), y_f(ymax)))
+ax2.plot([],[])
+ax2.set_yscale('log')
+ax2.tick_params(axis='both', labelsize=bigsize)
+ax2.set_xlim(2,3000)
+
+
+plt.tight_layout()
+
+
+#plt.show()
+plt.savefig("lum_tnu.png", dpi=500)
