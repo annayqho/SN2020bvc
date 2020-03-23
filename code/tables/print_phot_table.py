e49ef04c895be49a0654f15e2bd12a4e073a5816
annayqho <annayqho@gmail.com>
2020-03-23 12:04:10 -0700

    get the UVOT phot from files

--- code/tables/print_phot_table.py
+++ code/tables/print_phot_table.py
@@ -5,8 +5,6 @@ from math import floor, log10
 from astropy.time import Time
 from astropy.cosmology import Planck15
 import sys
-sys.path.append("/Users/annaho/Dropbox/Projects/Research/ZTF18abukavn/code")
-from load_lc import get_uv_lc
 
 def round_sig(x, sig=2):
     print(x)
@@ -20,13 +18,13 @@ def ndec(num):
     return len(dec)
 
 
-d = Planck15.luminosity_distance(z=0.03154).cgs.value
+d = Planck15.luminosity_distance(z=0.025201).cgs.value
 
 headings = np.array(
         ['Date (JD)', '$\Delta t$', 'Instrument', 'Filter', 
          'AB Mag', 'Error in AB Mag'])
 label = "uvot-phot"
-caption = "Optical and ultraviolet photometry for SN2018gep"
+caption = "Optical and ultraviolet photometry for SN\,2020bvc"
 
 # Print the table headers
 ncol = len(headings)
@@ -55,24 +53,27 @@ outputf.write("\\tablehead{ %s } \n" %colheadstr)
 outputf.write("\\tabletypesize{\scriptsize} \n")
 outputf.write("\startdata \n")
 
-datadir = "/Users/annaho/Dropbox/Projects/Research/ZTF18abukavn/data/phot"
-dat = np.loadtxt(datadir + "/ZTF18abukavn_opt_phot.dat",
-delimiter=' ', dtype=str)
-tel = dat[:,0]
-mjd = Time(dat[:,1].astype(float), format='mjd')
-t0 = Time(2458370.6473, format='mjd')
-dt = (mjd-t0).value
-filt = dat[:,2]
-mag = dat[:,3].astype(float)
-emag = dat[:,4].astype(float)
+dat = ascii.read("../../data/marshal_lc.txt")
+uvdat = ascii.read("../../data/UVOT_hostsub.ascii")
+uvt = uvdat['MJD']+2400000.5
+uvdt = uvt-t0
+uvfilt = uvdat['FILTER']
+uvflux = uvdat['AB_FNU_mJy']
+uveflux = uvdat['AB_FNU_mJy_ERRM']
 
-dt_uv, filt_uv, fnu_mjy_uv, efnu_mjy_uv = get_uv_lc()
-mjd = np.append(mjd.value, Time(dt_uv + t0.value, format='mjd').value)
-dt = np.append(dt, dt_uv)
-filt = np.append(filt, filt_uv)
-mag = np.append(mag, -2.5*np.log10(fnu_mjy_uv*1E-3/3631))
-emag = np.append(emag, efnu_mjy_uv/fnu_mjy_uv)
-tel = np.append(tel, np.array(['UVOT']*len(dt_uv)))
+t = dat['jdobs']
+dt = t-t0
+mag = dat['magpsf']
+emag = dat['sigmamagpsf']
+instr = dat['instrument']
+filt = dat['filter']
+
+# mjd = np.append(mjd.value, Time(dt_uv + t0.value, format='mjd').value)
+# dt = np.append(dt, dt_uv)
+# filt = np.append(filt, filt_uv)
+# mag = np.append(mag, -2.5*np.log10(fnu_mjy_uv*1E-3/3631))
+# emag = np.append(emag, efnu_mjy_uv/fnu_mjy_uv)
+# tel = np.append(tel, np.array(['UVOT']*len(dt_uv)))
 
 for ii in np.arange(len(dt)):
     # Convert the flux into a fluxstr
