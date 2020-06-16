154d5faefa84b1aef4f509730e62e7c6cc7ee9b6
annayqho <annayqho@gmail.com>
2020-06-16 13:38:18 -0700

    add r-band peak with a cross

--- code/plots/fig3_plot_lc.py
+++ code/plots/fig3_plot_lc.py
@@ -19,47 +19,6 @@ from get_lc import *
 zp = 2458883.17
 
 
-def plot_inset():
-    # zoomed-in window showing the earliest non-detection and detection
-    axins = inset_axes(
-            ax, 2, 1, loc=1,
-            bbox_to_anchor=(0.87,0.98),
-            bbox_transform=ax.transAxes)
-    choose = np.logical_and(det, band)
-    axins.errorbar(
-        dt[choose]*24, mag[choose], emag[choose], fmt='s', ms=6,
-        mec=rcol, mfc=rcol, c=rcol, label='r', zorder=9)
-    choose = np.logical_and(nondet, band)
-    axins.arrow(
-            2458370.6408-zp, 19.97, 0, 0.5, length_includes_head=True,
-            head_width=0.2, head_length=0.3, fc='k', ec='k')
-    band = filt=='g'
-    choose = np.logical_and(np.logical_and(det, band), dt*24 < 3)
-    axins.errorbar(
-            dt[choose]*24, mag[choose], emag[choose],
-            fmt='o', ms=5, mec='#57106e', mfc='white', c='#57106e', label='g')
-
-    # fit a line to this early g-band data
-    out = np.polyfit(dt[choose]*24, mag[choose], deg=1, w=1/emag[choose])
-    m,b = out
-    dt_plt = np.linspace(-1,3)
-    y_plt = m*dt_plt + b
-    axins.plot(dt_plt, y_plt, ls='--', c='k', lw=0.5)
-    axins.text(0.5, 0.5, "31.2 mag/day", fontsize=12, transform=axins.transAxes,
-            verticalalignment='top')
-
-    axins.set_xlim(-0.1,3)
-    axins.set_ylim(18,21)
-    axins.tick_params(axis='both', labelsize=12)
-    axins.set_xlabel(r"Hours since $t_0$", fontsize=12)
-    axins.invert_yaxis()
-    ax.plot([-1, -1], [21, 18], c='k', lw=0.5)
-    ax.plot([1, 1], [21, 18], c='k', lw=0.5)
-    ax.plot([-1, 1], [18, 18], c='k', lw=0.5)
-    ax.plot([-1, 1], [21, 21], c='k', lw=0.5)
-    #mark_inset(ax, axins, loc1=2, loc2=4, fc="none", ec="0.5")
-     
-
 def merge_lc():
     # get optical light curves 
     t, mag, emag, maglim, filt, inst = get_opt_lc()
@@ -92,6 +51,7 @@ def plot_lc():
     fig,axarr = plt.subplots(
             4, 3, figsize=(8,8), sharex=True, sharey=True)
 
+    bands = ['UVW2', 'UVM2', 'UVW1', 'U', 'u', 'B', 'g', 'V', 'r', 'i', 'z']
     for ii,use_f in enumerate(bands):
         ax = axarr.reshape(-1)[ii]
         choose = np.logical_and(det, filt == use_f)
@@ -108,18 +68,13 @@ def plot_lc():
                     dt[choose][order], mag[choose][order]-ext[f], 
                     c='lightgrey', alpha=0.7, zorder=0)
 
-        # for each panel, show the last non-detection (which was r-band)
+        # for each panel, show the last non-detection from ATLAS (which was o-band)
         ax.arrow(
-                2458370.6408-zp, 20.47, 0, 0.5, length_includes_head=True,
-                head_width=2, head_length=0.2, fc='r', ec='r', zorder=10)
-        # ax.arrow(
-        #         2458370.6408-zp, 19.97, 0, 0.5, length_includes_head=True,
-        #         head_width=0.05, head_length=0.2, fc='k', ec='k')
-
+                2458883.17-zp, 19.4, 0, 0.5, length_includes_head=True,
+                head_width=2, head_length=0.2, fc='k', ec='k', zorder=10)
         # for each panel, show the r-band peak with a cross
-        print(2458374.65-zp)
         ax.scatter(
-                2458374.65-zp, 16.3, marker='+', c='r', zorder=10) 
+                2458896-zp, 16.3, marker='+', c='r', zorder=10) 
 
         ax.yaxis.set_tick_params(labelsize=14)
         ax.xaxis.set_tick_params(labelsize=14)
@@ -154,7 +109,7 @@ def plot_lc():
     axarr.reshape(-1)[-1].set_visible(False)
     plt.subplots_adjust(hspace=0, wspace=0)
     ax.set_xlim(-5, 35)
-    ax.set_ylim(15, 21)
+    ax.set_ylim(15, 22.5)
     ax.invert_yaxis()
     fig.text(0.5, 0.04, 
         r"Days since $t_0=$JD 2458370.6473 (UT 2018 Sept 09.15)", 
